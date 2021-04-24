1f62e64da96ed71176756995fc396be3b86c1631
Hugo Beaucamps <hugo.beaucamps@jooxter.com>
2021-04-24 20:05:57 +0200

    more movable player

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -3,31 +3,17 @@ class PlayerLogic{
   private PlayerData playerData;
   private PlayerSimulation playerSimulation;
 
-  private Vector2 vel;
-  private Vector2 acc;
-  private Vector2 pos;
-
   private Vector2 currentMovement;
 
   public PlayerLogic(PlayerData playerData, PlayerSimulation playerSimulation){
     this.playerData = playerData;
     this.playerSimulation = playerSimulation;
   }
-  private void addForce(Vector2 force){
-    this.acc += force;
-  }
 
   public void Move(float movement){
     currentMovement = new Vector2(movement,0);
   }
   public void FixedUpdate(float deltaTime){
-      this.addForce(currentMovement*playerData.accMultiplier);
-      this.addForce(-this.vel* playerData.deceleration);
-      this.pos += this.vel;
-      this.acc = Vector2.ClampMagnitude(this.acc,playerData.maxAcc);
-      this.vel += this.acc;
-      this.vel = Vector2.ClampMagnitude(this.vel,playerData.maxSpeed);
-      this.acc *= 0;
-      playerSimulation.MovePlayer(vel);
+      playerSimulation.MovePlayer(currentMovement * deltaTime, this.playerData);
   }
 }
--- Assets/Scripts/PlayerSimulation.cs
+++ Assets/Scripts/PlayerSimulation.cs
@@ -1,15 +1,32 @@
 using UnityEngine;
 
 class PlayerSimulation {
+  private Vector2 acc;
+  private Vector2 pos;
   private Transform player;
+
+private void addForce(Vector2 force){
+    this.acc += force;
+  }
 
   public PlayerSimulation(Transform player){
     this.player = player;
   }
-  public void MovePlayer(Vector2 movement){
+  public void MovePlayer(Vector2 movement, PlayerData playerData) {
     Rigidbody2D rb = player.gameObject.GetComponent<Rigidbody2D>();
-    Vector3 velocity = rb.velocity;
-    velocity = movement;
-    rb.velocity= velocity;
+    Vector2 vel = rb.velocity;
+    this.addForce(movement * playerData.accMultiplier);
+    this.addForce(new Vector2(-vel.x,0) * playerData.deceleration);
+    this.pos += vel;
+    this.acc = Vector2.ClampMagnitude(this.acc, playerData.maxAcc);
+    vel += this.acc;
+    //this.vel = Vector2.ClampMagnitude(this.vel, playerData.maxSpeed);
+    Debug.Log(vel);
+    rb.velocity = vel;
+    resetAcc();
+  }
+
+  private void resetAcc() {
+    this.acc *= 0;
   }
 }
