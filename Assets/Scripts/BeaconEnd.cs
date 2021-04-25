c26f65c24416bd3a8069b3de94ea87b4f47d3f8c
zessirb <zessirb@gmail.com>
2021-04-25 11:12:28 +0200

    End beacon ready to fire event

---
+++ Assets/Scripts/BeaconEnd.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeaconEnd : MonoBehaviour
+{
+  private bool playerInReach;
+
+  private void Start() {
+    playerInReach = false;
+  }
+
+  private void OnTriggerEnter2D(Collider2D collider) {
+    Debug.Log(collider.name);
+    if (collider.CompareTag("Player"))
+      playerInReach = true;
+  }
+  private void OnTriggerExit2D(Collider2D collider) {
+    if (collider.CompareTag("Player"))
+      playerInReach = false;
+  }
+
+  public bool isPlayerInReach() {
+    return playerInReach;
+  }
+}
--- Assets/Scripts/BeaconGenerator.cs
+++ Assets/Scripts/BeaconGenerator.cs
@@ -17,12 +17,24 @@ public class BeaconGenerator : MonoBehaviour
     }
   }
 
+  private BeaconEnd _endBeacon;
+  private BeaconEnd endBeacon {
+    get {
+      if (_endBeacon == null)
+        _endBeacon = GetComponentInChildren<BeaconEnd>();
+      return _endBeacon;
+    }
+  }
+
   public void GenerateBeacon(InputAction.CallbackContext context) {
     if (context.performed)
       PlaceBeacon(player.transform.position);
   }
 
   private void PlaceBeacon(Vector2 position) {
-    Instantiate(beaconPrefab, position, transform.rotation, transform);
+    if (endBeacon == null || !endBeacon.isPlayerInReach())
+      Instantiate(beaconPrefab, position, transform.rotation, transform);
+    else
+    Debug.Log("End");
   }
 }
