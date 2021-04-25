6d1747f9faad35bd740c8e252731230add7b89ec
zessirb <zessirb@gmail.com>
2021-04-25 11:57:25 +0200

    Added beacon placing reach limit

---
+++ Assets/Scripts/Beacons/BeaconGenerator.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BeaconGenerator : MonoBehaviour
+{
+  [SerializeField]
+  private GameObject beaconPrefab;
+
+  private List<BeaconReach> beaconReachs;
+
+  private Transform _player;
+  private Transform player {
+    get {
+      if (_player == null)
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+      return _player;
+    }
+  }
+
+  private BeaconEnd _endBeacon;
+  private BeaconEnd endBeacon {
+    get {
+      if (_endBeacon == null)
+        _endBeacon = GetComponentInChildren<BeaconEnd>();
+      return _endBeacon;
+    }
+  }
+
+  public BeaconGenerator() {
+    beaconReachs = new List<BeaconReach>();
+  }
+
+  public void GenerateBeacon(InputAction.CallbackContext context) {
+    if (context.performed) {
+      GameObject newBeacon = PlaceBeacon(player.transform.position);
+      if (newBeacon != null)
+        beaconReachs.Add(newBeacon.GetComponentInChildren<BeaconReach>());
+    }
+  }
+
+  private GameObject PlaceBeacon(Vector2 position) {
+    if (endBeacon != null && endBeacon.isPlayerInReach()) {
+      Debug.Log("End");
+      return null;
+    }
+    foreach (BeaconReach reach in beaconReachs) {
+      if (reach != null && reach.isPlayerInReach())
+        return null;
+    }
+    return Instantiate(beaconPrefab, position, transform.rotation, transform);
+  }
+}
