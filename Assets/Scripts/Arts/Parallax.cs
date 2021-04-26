7915adb147d73eddb6c2f1041e3e8cd8f355b4e9
greet <greetschin@hotmail.fr>
2021-04-26 12:21:25 +0200

    Fix auto asiugn Camera for paralax

--- Assets/Scripts/Arts/Parallax.cs
+++ Assets/Scripts/Arts/Parallax.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
   private float startpos;
-  public GameObject cam;
+  Camera m_MainCamera;
   public float parallaxEffect;
 
     // Start is called before the first frame update
     void Start()
     {
+    m_MainCamera = Camera.main;
+    m_MainCamera.enabled = true;
     startpos = transform.position.x;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-    float dist = (cam.transform.position.x * parallaxEffect);
+    float dist = (m_MainCamera.transform.position.x * parallaxEffect);
     transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
     }
 }
