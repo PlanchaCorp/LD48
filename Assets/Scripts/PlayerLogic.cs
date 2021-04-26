fcfc7e99d7f0486682bfe98147b7c40f573f15d9
Zessirb <zessirb@gmail.com>
2021-04-26 10:16:13 +0200

    Adding music and jump sound

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -3,11 +3,17 @@ class PlayerLogic{
   private PlayerData playerData;
   private PlayerSimulation playerSimulation;
   private Vector2 currentMovement;
-private Transform groundcheck;
-  public PlayerLogic(PlayerData playerData, PlayerSimulation playerSimulation,Transform groundCheck){
+  private Transform groundcheck;
+  private SoundManager soundManager;
+
+  public PlayerLogic(PlayerData playerData, PlayerSimulation playerSimulation, Transform groundCheck){
     this.playerData = playerData;
     this.playerSimulation = playerSimulation;
     this.groundcheck = groundCheck;
+  }
+
+  public void AttachSoundManager(SoundManager manager) {
+    this.soundManager = manager;
   }
 
   public void Move(float movement){
@@ -20,17 +26,18 @@ private Transform groundcheck;
       } else {
         playerSimulation.aerialFriction();
       }
-       playerSimulation.StepMovementSimuation();
+      playerSimulation.StepMovementSimuation();
   }
 
   public bool isGrounded(){
     Collider2D[] groundColliders = Physics2D.OverlapCircleAll(groundcheck.position, playerData.feetRadius, playerData.whatIsGround);
-     return groundColliders.Length > 0;
+    return groundColliders.Length > 0;
   }
   public void Jump(){
     if(isGrounded()){
       float force = this.playerData.jumpForce;
       playerSimulation.Jump(force);
+      soundManager.Jump();
     }
   }
 }
