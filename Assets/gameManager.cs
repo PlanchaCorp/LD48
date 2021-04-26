aaaa6b7abb7213f424eae8fdd92cccd995e4cefa
Raiponz <spooki.leserpent@orange.fr>
2021-04-27 01:34:08 +0200

    add event system

--- Assets/gameManager.cs
+++ Assets/gameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class gameManager : MonoBehaviour
 {
   public void OnclickReset(){
+    Debug.Log("reset");
     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
   }
 }
