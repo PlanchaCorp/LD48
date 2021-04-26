4e5218ea6812d49cfe172fb7378277661ecd590b
Raiponz <spooki.leserpent@orange.fr>
2021-04-26 16:40:16 +0200

    update user prefab

--- Assets/ArmRotation.cs
+++ Assets/ArmRotation.cs
@@ -10,8 +10,13 @@ public class ArmRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var angle = Vector2.Angle(anchor.position,transform.position);
-        Debug.Log(angle);
+        var angle = AngleBetweenVector2(transform.position,anchor.position);
         transform.eulerAngles = new Vector3(0,0, angle);
     }
+    private float AngleBetweenVector2(Vector2 vec1, Vector2 vec2)
+ {
+         Vector2 diference = vec2 - vec1;
+         float sign = (vec2.y < vec1.y)? -1.0f : 1.0f;
+         return Vector2.Angle(Vector2.right, diference) * sign;
+     }
 }
