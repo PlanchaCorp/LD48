692aacdc3bdecbc65fdcd8a51e44a302a7b2166b
Raiponz <spooki.leserpent@orange.fr>
2021-04-26 18:36:55 +0200

    too swaggy camera

--- Assets/FollowBeacons.cs
+++ Assets/FollowBeacons.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class FollowBeacons : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Transform lookAt;
+    private Transform follow;
+
   public void OnBeaconPlaced(GameObject beacon){
-    GetComponent<Cinemachine.CinemachineVirtualCamera>().Follow = beacon.transform;
+    GetComponent<CinemachineVirtualCamera>().Follow = beacon.transform;
+    follow = GetComponent<CinemachineVirtualCamera>().Follow;
+  }
+  void Start(){
+      lookAt = GetComponent<CinemachineVirtualCamera>().LookAt;
+      follow = GetComponent<CinemachineVirtualCamera>().Follow;
+  }
+  void Update(){
+
+    Debug.Log(lookAt.position);
+    CinemachineFramingTransposer comp = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineFramingTransposer>();
+    comp.m_TrackedObjectOffset = follow.position - lookAt.position;
   }
 }
